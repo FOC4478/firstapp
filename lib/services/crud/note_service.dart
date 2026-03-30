b01c6258b1c6649b691e740d4493564f2ae9e1d8
innocent okekem <iokekem@gmail.com>
2026-03-30 12:18:48 +0100

    made changes

--- lib/services/crud/note_service.dart
+++ lib/services/crud/note_service.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:sqflite/sqflite.dart';
 import 'package:path_provider/path_provider.dart'
     show MissingPlatformDirectoryException, getApplicationDocumentsDirectory;
@@ -5,23 +6,148 @@ import 'package:path/path.dart' show join;
 
 import 'package:flutter/foundation.dart';
 
-class DatabaseAlreadyOpenException implements Exception {}
+class DatabaseAlreadyOpen implements Exception {}
 
-class UnableToGetDoucumentDirectoryException implements Exception {}
+class UnableToGetDoucumentDirectory implements Exception {}
+
+class DatabaseIsNotOpen implements Exception {}
+
+class CouldNotDeleteUser implements Exception {}
+
+class UserAlreadyExists implements Exception {}
+
+class CouldNotFindUser implements Exception {}
 
 class NotesService {
   Database? _db;
+
+  Future<DatabaseNote> createdNote({required DatabaseUser owner}) async {
+    final db = _getDatabaseOrThrow();
+
+    final dbUser = await getUser(email: owner.email);
+    if (dbUser != owner) {
+      throw CouldNotFindUser();
+    }
+
+    const text = '';
+
+    final noteId = await db.insert(userTable, {
+      userIdColumn: owner.id,
+      textColumn: text,
+      isSyncedWithCloudColumn: 1,
+    });
+
+    final note = DatabaseNote(
+      id: noteId,
+      userId: owner.id,
+      text: text,
+      isSyncedWithCloud: true,
+    );
+
+    return note;
+  }
+
+  Future<DatabaseUser> getUser({required String email}) async {
+    final db = _getDatabaseOrThrow();
+    final results = await db.query(
+      userTable,
+      limit: 1,
+      where: 'email = ?',
+      whereArgs: [email.toLowerCase()],
+    );
+
+    if (results.isEmpty) {
+      throw CouldNotFindUser();
+    } else {
+      return DatabaseUser.fromRow(results.first);
+    }
+  }
+
+  Future<DatabaseUser> createUser({required String email}) async {
+    final db = _getDatabaseOrThrow();
+    final results = await db.query(
+      userTable,
+      limit: 1,
+      where: 'email = ?',
+      whereArgs: [email.toLowerCase()],
+    );
+    if (results.isNotEmpty) {
+      throw UserAlreadyExists();
+    }
+
+    final userId = await db.insert(userTable, {
+      emailColumn: email.toLowerCase(),
+    });
+
+    return DatabaseUser(id: userId, email: email);
+  }
+
+  Future<void> deleteUser({required String email}) async {
+    final db = _getDatabaseOrThrow();
+    final deletedCount = await db.delete(
+      userTable,
+      where: 'email = ?',
+      whereArgs: [email.toLowerCase()],
+    );
+    if (deletedCount != 1) {
+      throw CouldNotDeleteUser();
+    }
+  }
+
+  Database _getDatabaseOrThrow() {
+    final db = _db;
+    if (db == null) {
+      throw DatabaseIsNotOpen();
+    } else {
+      return db;
+    }
+  }
+
+  Future<void> close() async {
+    final db = _db;
+    if (db == null) {
+      throw DatabaseIsNotOpen;
+    } else {
+      await db.close();
+      _db = null;
+    }
+  }
+
   Future<void> open() async {
     if (_db != null) {
-      throw DatabaseAlreadyOpenException();
+      throw DatabaseAlreadyOpen();
     }
     try {
       final docPath = await getApplicationDocumentsDirectory();
       final dbPath = join(docPath.path, dbName);
       final db = await openDatabase(dbPath);
       _db = db;
+      // create usertable
+      const createUserTable = '''
+          CREATE TABLE IF NOT EXISTS "User" (
+	        "id"	INTEGER NOT NULL,
+	        "email"	TEXT NOT NULL UNIQUE,
+	        PRIMARY KEY("id" AUTOINCREMENT)
+             ); 
+             ''';
+      await db.execute(createUserTable);
+
+      /// create note table
+
+      const createNoteTable = '''
+           CREATE TABLE IF NOT EXISTS "note" (
+	         "id"	INTEGER NOT NULL,
+	       "user__id"	INTEGER NOT NULL,
+	       "text"	TEXT,
+	       "is_synced_with_c;oud"	INTEGER NOT NULL DEFAULT 0,
+	       PRIMARY KEY("id" AUTOINCREMENT),
+	      FOREIGN KEY("user__id") REFERENCES "User"("id")
+         );
+       ''';
+
+      await db.execute(createNoteTable);
     } on MissingPlatformDirectoryException {
-      throw UnableToGetDoucumentDirectoryException();
+      throw UnableToGetDoucumentDirectory();
     }
   }
 }
